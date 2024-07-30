080c3207dcd95111484f61c1dabdb8fc9fcf05fa
Dominik Sakic <dominik.sakic@yahoo.com>
2024-07-30 17:13:59 +0900

    feat: build a thread to run the game loop

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,7 +3,7 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     //Screen Settings
     final int originalTileSize = 16; // 16x16
@@ -15,10 +15,23 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize* maxScreenRow;
 
+    Thread gameThread;
+
+
     public GamePanel(){
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void StartGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
